09b608e0f13a96fef9f121777d8a929cdb96048d
Sudnya Diamos <mailsudnya@gmail.com>
2014-01-04 18:28:32 -0800

    ENH: added random initialization to the model, reduced resolution

--- minerva/scripts/kaggle/cats-dogs.py
+++ minerva/scripts/kaggle/cats-dogs.py
@@ -9,9 +9,13 @@ import itertools
 import subprocess
 
 unsupervised_learning_reduction_factor = 100
-unsupervised_learning_db = 'unsupervised_learning_db.txt'
-training_db = 'training_db.txt'
-cross_val_db = 'cross_val_db.txt'
+#unsupervised_learning_db = 'unsupervised_learning_db.txt'
+#training_db = 'training_db.txt'
+#cross_val_db = 'cross_val_db.txt'
+
+unsupervised_learning_db = '../../examples/faces-training-database.txt'
+training_db = '../../examples/faces-training-database.txt'
+cross_val_db = '../../examples/faces-test-database.txt'
 
 def populate_db(path, freq):
     training_file = open(training_db, 'w')
@@ -72,7 +76,7 @@ def call_minerva(p, options, count):
     logs = ' -L ClassifierEngine,UnsupervisedLearnerEngine,LearnerEngine,FinalClassifierEngine'
 
     model = 'models/' + str(count) + '.tgz'
-    redirect_log = ' > ' + str(count) + '.log'
+    redirect_log = ' | tee ' + str(count) + '.log'
     # create model
     create = cmd + ' -n -m ' + model + redirect_log + '.create'
     process = subprocess.Popen(create, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -80,19 +84,19 @@ def call_minerva(p, options, count):
     (stdOutData, stdErrData) = process.communicate()
     
     # unsupervised learning
-    unsupervised = cmd + logs + ' -i ' + training_db + ' -l -m ' + model + redirect_log + '.unsupervised'
+    unsupervised = cmd + logs + ' -i ' + training_db + ' -l -m ' + model #+ redirect_log + '.unsupervised'
     process = subprocess.Popen(unsupervised, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     print "Unsupervised : ", unsupervised
     (stdOutData, stdErrData) = process.communicate()
  
     # supervised learning
-    supervised = cmd + logs + ' -i ' + training_db + ' -t -m ' + model + redirect_log + '.supervised'
+    supervised = cmd + logs + ' -i ' + training_db + ' -t -m ' + model #+ redirect_log + '.supervised'
     process = subprocess.Popen(supervised, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     print "Supervised : ", supervised
     (stdOutData, stdErrData) = process.communicate()
 
     # test (we use cross val set here)
-    test = cmd + logs + ' -i ' + cross_val_db + ' -c -m ' + model + redirect_log + '.test'
+    test = cmd + logs + ' -i ' + cross_val_db + ' -c -m ' + model #+ redirect_log + '.test'
     process = subprocess.Popen(test, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     print "Classifiy : ", test
     (stdOutData, stdErrData) = process.communicate()
@@ -117,20 +121,20 @@ def generate_search_space():
     for p in product:
         call_minerva(p, options, counter)
         counter += 1
-        if counter > 2:
+        if counter > 1:
             break
 
 
 
 def main():
     parser = argparse.ArgumentParser(description="Process commandline inputs")
-    parser.add_argument('-training',    help="path to directory containing training data", type=str, default='/Users/sudnya/Documents/checkout/git/binary-image-classifier/data/train')
+#    parser.add_argument('-training',    help="path to directory containing training data", type=str, default='/Users/sudnya/Documents/checkout/git/binary-image-classifier/data/train')
     parser.add_argument('-cv',          help="the frequency to populate cross validation for every training sample (1 cv for every 4 training samples -> 20%)", type=int, default=5)
     parser.add_argument('-x',           help="x resolution", type=int, default=48)
     parser.add_argument('-y',           help="y resolution", type=int, default=48)
     parser.add_argument('-trainingsize',help="number of training files to use", type=int, default=5)
     args = parser.parse_args()
-    populate_db(args.training, args.cv)
+#    populate_db(args.training, args.cv)
     generate_search_space()
 
 
