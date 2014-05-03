1b74c936faf3d9b1a4adf99bfa57a8d134c6e7e0
Sudnya Diamos <mailsudnya@gmail.com>
2014-05-03 13:22:04 -0700

    ENH: Added placeholder for GPU implementation of newton's method.

---
+++ minerva/minerva/optimizer/interface/GPULBFGSSolver.h
@@ -0,0 +1,38 @@
+/*	\file   GPULBFGSSolver.h
+	\date   Saturday August 10, 2013
+	\author Gregory Diamos <solusstultus@gmail.com>
+	\brief  The header file for the GPULBFGSSolver class.
+*/
+
+#pragma once
+
+// Minvera Includes
+#include <minerva/optimizer/interface/GeneralDifferentiableSolver.h>
+
+namespace minerva
+{
+
+namespace optimizer
+{
+
+class GPULBFGSSolver : public GeneralDifferentiableSolver
+{
+public:
+	virtual ~GPULBFGSSolver();
+
+public:
+	virtual float solve(BlockSparseMatrixVector& inputs, 
+		const CostAndGradientFunction& callback);
+
+public:
+	static double getMemoryOverhead();
+
+public:
+	static bool isSupported();
+
+};
+
+}
+
+}
+
