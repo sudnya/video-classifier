e9464d182c85ed60ae1140c2c57c412dd2fc0cf2
Gregory Diamos <solusstultus@gmail.com>
2016-05-11 19:50:23 -0700

    ADD: Added sample header file for fft operations.

---
+++ lucius/lucius/matrix/interface/FftOperations.h
@@ -0,0 +1,18 @@
+#pragma once
+
+// Forward Declarations
+namespace lucius { namespace matrix { class Matrix;    } }
+namespace lucius { namespace matrix { class Dimension; } }
+
+namespace lucius
+{
+namespace matrix
+{
+
+void fft(Matrix& result, const Matrix& signal, const Dimension& transformedDimensions);
+Matrix fft(const Matrix& signal, const Dimension& transformedDimensions);
+
+}
+}
+
+
